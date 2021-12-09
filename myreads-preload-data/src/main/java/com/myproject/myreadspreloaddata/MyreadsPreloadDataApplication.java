79897b96ec0368df304f4b3121aa98fe28948c99
IlyasBouziane <ilyas.bouziane@um5s.net.ma>
2021-12-09 22:52:40 +0100

    init the spring app for data preload

---
+++ myreads-preload-data/src/main/java/com/myproject/myreadspreloaddata/MyreadsPreloadDataApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.myreadspreloaddata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyreadsPreloadDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyreadsPreloadDataApplication.class, args);
+	}
+
+}
---
+++ myreads-preload-data/src/test/java/com/myproject/myreadspreloaddata/MyreadsPreloadDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.myreadspreloaddata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyreadsPreloadDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
