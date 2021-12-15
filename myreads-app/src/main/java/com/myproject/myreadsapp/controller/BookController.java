358003b1d3f9c88d2f8c658570757933e05f443d
IlyasBouziane <ilyas.bouziane@um5s.net.ma>
2021-12-15 21:26:15 +0100

    implemented the book cover & web rendering of book details

--- myreads-app/src/main/java/com/myproject/myreadsapp/controller/BookController.java
+++ myreads-app/src/main/java/com/myproject/myreadsapp/controller/BookController.java
@@ -16,12 +16,20 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
+    private final String COVER_IMAGE_ROOT="https://covers.openlibrary.org/b/id/";
+
     @GetMapping(value = "/books/{bookId}")
     public String getBookById(@PathVariable String bookId, Model model){
         Optional<Book> book = bookRepository.findById(bookId);
+        String imageUrl;
         if(book.isPresent()){
+            if(book.get().getCoversIds().size()>0 && book.get().getCoversIds()!=null){
+                imageUrl = COVER_IMAGE_ROOT+book.get().getCoversIds().get(0)+"-L.jpg";
+            }else{
+                imageUrl ="/images/no-image.jpg";
+            }
+            model.addAttribute("coverImage", imageUrl );
             model.addAttribute("book", book.get());
-            System.out.println(book.get().getBookName());
             return "book/book";
         } else {
             return "book/book-not-found";
