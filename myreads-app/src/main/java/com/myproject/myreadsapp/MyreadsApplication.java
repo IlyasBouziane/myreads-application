8b42242781d9f2da0b6c21cde596b9eaeae54a0b
IlyasBouziane <ilyas.bouziane@um5s.net.ma>
2021-12-17 23:47:54 +0100

    implemented book search feature

--- myreads-app/src/main/java/com/myproject/myreadsapp/MyreadsApplication.java
+++ myreads-app/src/main/java/com/myproject/myreadsapp/MyreadsApplication.java
@@ -3,14 +3,12 @@ package com.myproject.myreadsapp;
 import java.nio.file.Path;
 
 import com.myproject.myreadsapp.connection.DatastaxAstraConfigs;
-import com.myproject.myreadsapp.controller.BookController;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
--- myreads-app/src/main/java/com/myproject/myreadsapp/controller/SearchController.java
+++ myreads-app/src/main/java/com/myproject/myreadsapp/controller/SearchController.java
@@ -2,21 +2,26 @@ package com.myproject.myreadsapp.controller;
 
 
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.myproject.myreadsapp.model.SearchResult;
+import com.myproject.myreadsapp.model.SearchResultBooks;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import reactor.core.publisher.Mono;
-
 @Controller
 public class SearchController {
 
     private WebClient webClient;
+    private final String COVER_IMAGE_ROOT="https://covers.openlibrary.org/b/id/";
+
     
     public SearchController(WebClient.Builder wBuilder ){
         this.webClient = wBuilder
@@ -27,13 +32,28 @@ public class SearchController {
 
     @GetMapping(value = "/search")
     public String getSearchBook(@RequestParam String query,Model model){
-        SearchResult result = webClient.get()
+        SearchResult response = webClient.get()
             .uri("?q={query}",query)   
             .retrieve()
             .bodyToMono(SearchResult.class)
             .block();
+
+        List<SearchResultBooks> results = response.getDocs()
+                                            .stream()
+                                            .limit(10)
+                                            .map(res -> {
+                                                res.setKey(res.getKey().replace("/works/", ""));
+                                                String cover = res.getCover_i();
+                                                if(StringUtils.hasText(cover)){
+                                                    res.setCover_i(COVER_IMAGE_ROOT+cover+"-M.jpg");
+                                                }else{
+                                                    res.setCover_i("/images/no-image.jpg");
+                                                }
+                                                return res;
+                                            })
+                                            .collect(Collectors.toList());
         
-        model.addAttribute("result", result);
+        model.addAttribute("results", results);
         return "search";
     }
     
